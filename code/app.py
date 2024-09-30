b10d46717ed567d8df23f6fb18980cc68d124c15
Irfan <irfan.a.tamboli@gmail.com>
2024-09-30 12:53:27 +0300

    added flask-ngrok

--- code/app.py
+++ code/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app) 
 
 @app.route('/')
 def home():
-    return 'Welcome to Webpage'
+    return 'Welcome to sample Webpage'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8986)
+    app.run()
+#    app.run(host='127.0.0.1', port=8986)
