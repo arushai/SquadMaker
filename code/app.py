db35c190274467b51c604b1cf905cc1f1157e321
Irfan <irfan.a.tamboli@gmail.com>
2024-10-02 14:59:57 +0300

    updates

--- code/app.py
+++ code/app.py
@@ -3,11 +3,11 @@ from flask_ngrok import run_with_ngrok
 from pyngrok import ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app) 
+run_with_ngrok(app)             # Start ngrok when app is run
 
 @app.route('/')
 def home():
-    return 'Welcome to sample Webpage'
+    return 'Welcome to Box Cricket SquadMaker'
 
 if __name__ == '__main__':
     ngrok_tunnel = ngrok.connect(5000)
